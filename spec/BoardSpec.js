0bc20b91e18f856291e99aea6aaa6a1983aaacca
tritiatimmins <tritialtimmins@gmail.com>
2016-02-03 16:37:55 -0800

    We finished all of our specs except for rook/queen ones

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -5,30 +5,19 @@ describe("Board", function() {
   };
 
 
+  var verifyConflictTypes = function(expectedConflicts, matrix){
+    // The Board() constructor will accept a matrix and build that into a (Backbone) Board object (as defined in Board.js)
+    var board = new Board(matrix);
+    _.map('row col rooks majorDiagonal minorDiagonal queens'.split(' '), function(conflictType){
+      var conflictDetected = board['hasAny' + capitalize(conflictType) + 'Conflicts']();
+      var conflictExpected = _(expectedConflicts).contains(conflictType);
+      var message = conflictExpected ? 'should' : 'should not';
 
-var verifyConflictTypes = function(expectedConflicts, matrix){
-  // The Board() constructor will accept a matrix and build that into a (Backbone) Board object (as defined in Board.js)
-  var board = new Board(matrix);
-  //map takes in elements and directions
-  //conflictType is the value of each element htat is passed into map
-  _.map('row col rooks majorDiagonal minorDiagonal queens'.split(' '), function(conflictType){
-    //conflictDetected runs the board function and finds the correct board method based on the conflict type
-    var conflictDetected = board['hasAny' + capitalize(conflictType) + 'Conflicts']();
-    //conflictDetected runs the board function and finds the correct board method based on the conflict type
-    var conflictDetectedAny = board['has' + capitalize(conflictType) + 'ConflictAt']();
-    //checks to see if the value from Map is within the expectedConflicts that were passed in
-    var conflictExpected = _(expectedConflicts).contains(conflictType);
-    //message runs ternary where if conflictExpected evaluates to true, then returns should, else, should not
-    var message = conflictExpected ? 'should' : 'should not';
-    //('Should') find a (map value) and concatiates it to conflict and then runs the function that evaluates if the conflict detected is equal to the conflict expected
-    it(message + " find a " + conflictType + " conflict", function() {
-      expect(conflictDetected).to.be.equal(conflictExpected);
+      it(message + " find a " + conflictType + " conflict", function() {
+        expect(conflictDetected).to.be.equal(conflictExpected);
+      });
     });
-    it(message + "find a " + conflictType + " conflict", function(){
-      expect(conflictDetectedAny).to.be.equal(conflictExpected);
-    });
-  });
-};
+  };
 
   describe("Empty board", function() {
     verifyConflictTypes([''], [
--- src/Board.js
+++ src/Board.js
@@ -78,26 +78,36 @@
     // --------------------------------------------------------------
     //
     // test if a specific row on this board contains a conflict
+    rowLooperFunction: function(x) {
+      var row = this.get(x);
+
+      return _.reduce(row, function(memo, value) {
+        return memo + value;
+      });
+
+    },
+
     hasRowConflictAt: function(rowIndex) {
-      //console.log(this);
-        //var boardRowIndex = row[rowIndex];
-      var count = 0
-      //this.get(rowIndex);
-      for(var i = 0; i < boardRowIndex.length; i++) {    
-        if(boardRowIndex[i] > 0){
-          count ++;
-        };
+      var row = this.rowLooperFunction(rowIndex);
+      console.log(row);
+      if (row > 1) {
+        return true; // fixme
       }
-        if(count > 1){
-          return false; // fixme 
-        }else{
-          return true;
-        }
     },
 
     // test if any rows on this board contain conflicts
+    //make some kind of reference to what the conflicts are
+    //rather than the matrices themselves
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var row1 = this.rowLooperFunction(0);
+      var row2 = this.rowLooperFunction(1);
+      var row3 = this.rowLooperFunction(2);
+      var row4 = this.rowLooperFunction(3);
+
+      if (row1 > 1 || row2 > 1 || row3 > 1 || row4 > 1) {
+        return true;
+      } 
+        return false;
     },
 
 
@@ -106,12 +116,41 @@
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
+    columnLooperFunction: function(column) {
+      var row1 = this.get(0);
+      var row2 = this.get(1);
+      var row3 = this.get(2);
+      var row4 = this.get(3);
+      //var rows = row1 + ',' + row2 + ',' + row3 + ',' + row4;
+      var holder = [];
+
+      var columnMaker = function(column) {
+        holder.push(row1[column]);
+        holder.push(row2[column]);
+        holder.push(row3[column]);
+        holder.push(row4[column]);
+      }
+      columnMaker(column);
+
+      return _.reduce(holder, function(memo, value) {
+        return memo + value;
+      })
+    },
+
     hasColConflictAt: function(colIndex) {
       return false; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      var column1 = this.columnLooperFunction(0);
+      var column2 = this.columnLooperFunction(1);
+      var column3 = this.columnLooperFunction(2);
+      var column4 = this.columnLooperFunction(3);
+
+      if (column1 > 1 || column2 > 1 || column3 > 1 || column4 > 1 ) {
+        return true;
+      }
       return false; // fixme
     },
 
@@ -121,27 +160,110 @@
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
+    majorDiagonalChecker: function() {
+      //if we iterate over an array, to see diagonally we +1 to the index of the array
+      var diagonalHolder = [];
+      //lets look at them as rows
+      var row1 = this.get(0);
+      var row2 = this.get(1);
+      var row3 = this.get(2);
+      var row4 = this.get(3);
+
+      var diagonal1 = [row3[0], row4[1]];
+      var diagonal2 = [row2[0], row3[1], row4[2]];
+      var diagonal3 = [row1[0], row2[1], row3[2], row4[3]];
+      var diagonal4 = [row1[1], row2[2], row3[3]];
+      var diagonal5 = [row1[2], row2[3]];
+
+      diagonalHolder.push(_.reduce(diagonal1, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal2, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal3, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal4, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal5, function(memo, value) {
+        return memo + value;
+      }));
+
+      return diagonalHolder;
+    },
+
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+      var holder = [];
+
       return false; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      var hasDiagonal = this.majorDiagonalChecker();
+      for (var i = 0; i < hasDiagonal.length; i++) {
+        if (hasDiagonal[i] > 1) {
+          return true;
+        }
+      }
+
       return false; // fixme
     },
-
-
 
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
+    minorDiagonalChecker: function() {
+      var diagonalHolder = [];
+      //lets look at them as rows
+      var row1 = this.get(0);
+      var row2 = this.get(1);
+      var row3 = this.get(2);
+      var row4 = this.get(3);
+
+      var diagonal1 = [row1[1], row2[0]];
+      var diagonal2 = [row1[2], row2[1], row3[0]]; 
+      var diagonal3 = [row1[3], row2[2], row3[1], row4[0]];
+      var diagonal4 = [row2[3], row3[2], row4[1]];
+      var diagonal5 = [row3[3], row4[2]];
+
+      diagonalHolder.push(_.reduce(diagonal1, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal2, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal3, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal4, function(memo, value) {
+        return memo + value;
+      }));
+      diagonalHolder.push(_.reduce(diagonal5, function(memo, value) {
+        return memo + value;
+      }));
+      
+      return diagonalHolder;
+
+    },
+
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
+
       return false; // fixme
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
+      var hasDiagonal = this.minorDiagonalChecker();
+      for (var i = 0; i < hasDiagonal.length; i++) {
+        if (hasDiagonal[i] > 1) {
+          return true;
+        }
+      }
+
       return false; // fixme
     }
 
